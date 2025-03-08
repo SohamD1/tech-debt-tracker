e35c71605f799bd9f37ec2df514bae2e2fb34525
Soham Dave <davesoham14@gmail.com>
2025-03-08 14:09:10 -0500

    require => import

--- backend/index.js
+++ backend/index.js
@@ -1,31 +1,27 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const routes = require('./routes');
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import routes from "./routes.js";
 // Above are the required packages
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 // sets the port to listen on (default 5000)
 
-
 // middleware
 app.use(cors());
 app.use(bodyParser.json());
-app.use('/api/debt', routes); //Custom routes mounting
-app.get('/', (req, res) => {
-    res.send("Welcome to the Tech Debt TrackerAPI");
-  });
-
-
-  app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).json({ error: "Somethings wrong!" });
-  });
-// error handling ^
-
-app.listen(PORT,() => {
-    console.log(`Server is running on port ${PORT}`)
+app.use("/api/debt", routes); //Custom routes mounting
+app.get("/", (req, res) => {
+  res.send("Welcome to the Tech Debt TrackerAPI");
 });
 
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ error: "Somethings wrong!" });
+});
+// error handling ^
 
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
